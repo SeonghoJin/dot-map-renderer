87e90d49ce5953c62481ba125cf8ce7d9b8bf213
moretall <seongho4996@naver.com>
2022-07-06 18:05:59 +0900

    refactor(component): move files

--- packages/component/src/IComponent.ts
+++ packages/component/src/IComponent.ts
@@ -1,6 +1,6 @@
 import { Drawable } from './Drawable';
 import { IResizable } from './IResizable';
-import { IHitable } from '../IHitable';
+import { IHitable } from './IHitable';
 
 export interface IComponent extends Drawable, IResizable, IHitable {
 }
--- packages/component/src/IHitable.ts
+++ packages/component/src/IHitable.ts
@@ -1,6 +1,6 @@
 import { Collider } from '@dot-map-renderer/collider';
-import { Point } from '@dot-map-renderer/component';
-import { IComponent } from './src/IComponent';
+import { Point } from 'packages/component/src/index';
+import { IComponent } from './IComponent';
 
 export interface IHitable {
     interaction: boolean;

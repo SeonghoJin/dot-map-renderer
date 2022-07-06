91b0e656c9c2326f20ae6d365c4bbf94ba6e8df7
moretall <seongho4996@naver.com>
2022-07-06 19:57:55 +0900

    add packages/example

---
+++ packages/example/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import {Header} from "react-typescript-teamplate";
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

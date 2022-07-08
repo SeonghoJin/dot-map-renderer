b9ef093e56ba4969b9031336643e313476f6becc
moretall <seongho4996@naver.com>
2022-07-08 19:59:33 +0900

    fix(component): rendererContext도 할 수 있도록 수정

--- packages/component/src/IComponent.ts
+++ packages/component/src/IComponent.ts
@@ -1,8 +1,5 @@
 import { Drawable } from './Drawable';
 import { IResizable } from './IResizable';
-import { IHitable } from './IHitable';
-import { Collider } from '@dot-map-renderer/collider';
-import { Point } from './Point';
 import { RendererContext } from '@dot-map-renderer/canvas/src/RendererContext';
 
 export abstract class IComponent implements Drawable, IResizable {
@@ -12,6 +9,6 @@ export abstract class IComponent implements Drawable, IResizable {
 
   abstract resize(renderer: RendererContext): void;
 
-  update = (context: CanvasRenderingContext2D, canvas) => {};
-  start = (context: CanvasRenderingContext2D) => {};
+  update = (context: CanvasRenderingContext2D, rendererContext: RendererContext) => {};
+  start = (context: CanvasRenderingContext2D, rendererContext: RendererContext) => {};
 }

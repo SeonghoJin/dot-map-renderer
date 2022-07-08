3ae1ca3902f2f79a1896a39f187c57dbfbe3c861
moretall <seongho4996@naver.com>
2022-07-08 19:58:37 +0900

    fix: 환형 종속 문제 해결

--- packages/component/src/dot/DotFactory.ts
+++ packages/component/src/dot/DotFactory.ts
@@ -1,30 +1,26 @@
-import { DotType, CircleDot, RectDot } from '@dot-map-renderer/component';
+import { CircleDot } from './CircleDot';
 import { Dot } from './Dot';
+import { DotType } from './DotType';
+import { RectDot } from './RectDot';
 
 type CreateFunctionType = {
-    new (...args: any[]): Dot
+  new (...args: any[]): Dot;
 };
 
-export class DotFactory
-{
-    type: DotType;
-    createFunction: CreateFunctionType;
+export class DotFactory {
+  type: DotType;
+  createFunction: CreateFunctionType;
 
-    constructor(type: DotType)
-    {
-        this.type = type;
-        if (this.type === 'rectangular')
-        {
-            this.createFunction = RectDot;
-        }
-        else
-        {
-            this.createFunction = CircleDot;
-        }
+  constructor(type: DotType) {
+    this.type = type;
+    if (this.type === 'rectangular') {
+      this.createFunction = RectDot;
+    } else {
+      this.createFunction = CircleDot;
     }
+  }
 
-    create(x: number, y: number, size: number, gapSize: number, color: string): Dot
-    {
-        return new this.createFunction(x, y, size, gapSize, color);
-    }
+  create(x: number, y: number, size: number, gapSize: number, color: string): Dot {
+    return new this.createFunction(x, y, size, gapSize, color);
+  }
 }

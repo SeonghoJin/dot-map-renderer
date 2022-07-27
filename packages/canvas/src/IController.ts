f3941d779dcaf943a0b915850afecbee178e398e
moretall <seongho4996@naver.com>
2022-07-27 11:00:36 +0900

    feat(canvas): create addComponent API

--- packages/canvas/src/IController.ts
+++ packages/canvas/src/IController.ts
@@ -1,9 +1,11 @@
 import { Point } from '@dot-map-renderer/component';
 import { LineData } from '@dot-map-renderer/component/src/line/LineData';
+import { IComponent } from '@dot-map-renderer/component/src/IComponent';
 
 export interface IController {
   addAnchor: (points: Point) => void;
   addLine: (line: LineData) => void;
+  addComponent: (component: IComponent[]) => void;
   move: (moveY: number, moveX: number) => void;
   addZoom: (zoom: number) => void;
 }
--- packages/util/src/observer.d.ts
+++ packages/util/src/observer.d.ts
@@ -1,11 +1,11 @@
 export declare abstract class Observer<State> {
-  private subscribers;
-  protected state: State;
-  protected constructor(initState: State);
-  subscribe(subscriber: ISubscriber<State>): void;
-  notify(): void;
-  abstract changeState(state: State): void;
+    private subscribers;
+    protected state: State;
+    protected constructor(initState: State);
+    subscribe(subscriber: ISubscriber<State>): void;
+    notify(): void;
+    abstract changeState(state: State): void;
 }
 export interface ISubscriber<State = unknown> {
-  execute(data: State): void;
+    execute(data: State): void;
 }

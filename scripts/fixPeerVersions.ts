a241ced7f146349f1fced99ece5b72f2aef84ae0
moretall <seongho4996@naver.com>
2022-07-28 10:14:43 +0900

    feat(scripts): 정보 출력하도록 수정

--- scripts/fixPeerVersions.ts
+++ scripts/fixPeerVersions.ts
@@ -1,51 +1,55 @@
 import util from 'util';
 import fs from 'fs';
 import path from 'path';
-import workspacesRun from "workspaces-run";
+import workspacesRun from 'workspaces-run';
 import { Workspace } from 'workspaces-run/dist/types';
 
 interface PackageLike {
-    name: string;
-    dir: string;
-    config: {
-        version: string;
-        peerDependencies?: Record<string, string>;
-    }
+  name: string;
+  dir: string;
+  config: {
+    version: string;
+    peerDependencies?: Record<string, string>;
+  };
 }
 
 const writeFile = util.promisify(fs.writeFile);
 const writeJson = (file: string, data: unknown) => writeFile(file, `${JSON.stringify(data, null, 2)}\n`);
 
 async function main(): Promise<void> {
-    const versions: Record<string, string> = {};
-    const packagesWithPeers: PackageLike[] = [];
+  const versions: Record<string, string> = {};
+  const packagesWithPeers: PackageLike[] = [];
 
-    await workspacesRun({cwd: process.cwd()}, async (pkg: Workspace) => {
-        versions[pkg.name] = pkg.config.version;
+  await workspacesRun({ cwd: process.cwd() }, async (pkg: Workspace) => {
+    versions[pkg.name] = pkg.config.version;
 
-        if(pkg.config.peerDependencies) {
-            packagesWithPeers.push(pkg);
+    if (pkg.config.peerDependencies) {
+      packagesWithPeers.push(pkg);
+    }
+  });
+
+  const result = await Promise.allSettled(
+    packagesWithPeers.map((pkg) => {
+      const json = pkg.config;
+      const peers = json.peerDependencies;
+
+      for (const n in peers) {
+        if (n in versions) {
+          peers[n] = versions[n];
         }
-    })
+      }
 
-    const result = await Promise.allSettled(packagesWithPeers.map((pkg) => {
-        const json = pkg.config;
-        const peers = json.peerDependencies;
+      console.log(`write ${path.join(pkg.dir, 'package.json')} peerDependencies =`, peers);
 
-        for(const n in peers){
-            if(n in versions){
-                peers[n] = versions[n];
-            }
-        }
+      return writeJson(path.join(pkg.dir, 'package.json'), json);
+    }),
+  );
 
-        return writeJson(path.join(pkg.dir, 'package.json'), json);
-    }));
-
-    result.forEach((res) => {
-        if(res.status === 'rejected'){
-            console.error(res.reason);
-        }
-    })
+  result.forEach((res) => {
+    if (res.status === 'rejected') {
+      console.error(res.reason);
+    }
+  });
 }
 
 main();

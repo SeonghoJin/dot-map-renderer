f868018c7d73a2473b9ef5b991e4fb8e371daadf
moretall <seongho4996@naver.com>
2022-07-28 12:13:17 +0900

    build

--- packages/canvas/src/index.ts
+++ packages/canvas/src/index.ts
@@ -4,3 +4,7 @@ export * from './CanvasController';
 export * from './IRendererOption';
 export * from './IController';
 export * from './IInteraction';
+export * from './AnimationManager';
+export * from './CanAnimation';
+export * from './ComponentLayerAnimator';
+export * from './RendererContext';
--- packages/component/src/index.ts
+++ packages/component/src/index.ts
@@ -4,4 +4,5 @@ export * from './Canvas';
 export * from './Drawable';
 export * from './Polygon';
 export * from './Point';
+export * from './line';
 export * from './coordinateNormalization';
